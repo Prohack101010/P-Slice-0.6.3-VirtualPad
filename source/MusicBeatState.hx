aebb7a02c95a5f7905c2326c55e7b4d89c0d4f77
Prohack101010 <miracberaber@gmail.com>
2024-12-28 12:36:34 +0300

    Still Doesn't Work -_-

--- source/MusicBeatState.hx
+++ source/MusicBeatState.hx
@@ -39,6 +39,9 @@ class MusicBeatState extends FlxUIState
 		return PlayerSettings.player1.controls;
 		
 	#if TOUCH_CONTROLS_ALLOWED
+	public static var dpadMode:Map<String, FlxDPadMode>;
+	public static var actionMode:Map<String, FlxActionMode>;
+	
 	public static var checkHitbox:Bool = false;
 	
 	var _virtualpad:FlxVirtualPad;
@@ -133,6 +136,38 @@ class MusicBeatState extends FlxUIState
 	#end
 
 	override function create() {
+	    #if TOUCH_CONTROLS_ALLOWED
+	    // FlxDPadModes (for Mobile Controls)
+		dpadMode = new Map<String, FlxDPadMode>();
+		dpadMode.set("UP_DOWN", UP_DOWN);
+		dpadMode.set("LEFT_RIGHT", LEFT_RIGHT);
+		dpadMode.set("UP_LEFT_RIGHT", UP_LEFT_RIGHT);
+		dpadMode.set("LEFT_FULL", FULL); //1.0 Support
+		dpadMode.set("FULL", FULL);
+		dpadMode.set("RIGHT_FULL", RIGHT_FULL);
+		dpadMode.set("DUO", DUO);
+		dpadMode.set("NONE", NONE);
+			
+		actionMode = new Map<String, FlxActionMode>();
+		actionMode.set('E', E);
+		actionMode.set('A', A);
+		actionMode.set('B', B);
+		actionMode.set('D', D);
+		actionMode.set('P', P);
+		actionMode.set('X_Y', X_Y);
+		actionMode.set('A_B', A_B);
+		actionMode.set('A_C', A_C);
+		actionMode.set('A_B_C', A_B_C);
+		actionMode.set('A_B_E', A_B_E);
+		actionMode.set('A_B_E_C_M', A_B_E_C_M);
+		actionMode.set('A_B_X_Y', A_B_X_Y);
+		actionMode.set('A_X_Y', A_X_Y);
+		actionMode.set('B_X_Y', B_X_Y);
+		actionMode.set('A_B_C_X_Y', A_B_C_X_Y);
+		actionMode.set('A_B_C_X_Y_Z', A_B_C_X_Y_Z);
+		actionMode.set('NONE', NONE);
+		#end
+		
 		camBeat = FlxG.camera;
 
 		var skip:Bool = FlxTransitionableState.skipNextTransOut;
